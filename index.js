16eea8f8f2205cd1c07da5ebe734498a1f530b11
alairon <alai01@uoguelph.ca>
2019-01-01 13:53:12 -0500

    Basic implementation of game switching

--- index.js
+++ index.js
@@ -8,9 +8,11 @@
 // IPC
 const { ipcRenderer } = require('electron');
 
+const titles = require('./titles.json');
+
 const updateStatus = document.getElementById('updateStatus');
 updateStatus.addEventListener('click', () => {
-  ipcRenderer.send('updateStat', '525817582431764495');
+  ipcRenderer.send('updateStat', document.getElementById('titleName').value);
 });
 
 /* Unused IPC commands -- may be used in the future
@@ -24,14 +26,32 @@ ipcRenderer.on('update-result', function(event, arg) {
 });
 */
 
+function getclientAppID(title) {
+  let clientAppID;
+
+  switch(title) {
+    case 'Super Smash Bros. Ultimate':
+      clientAppID = titles.SmashUltimate.clientAppID;
+      break;
+    case 'Splatoon 2':
+      clientAppID = titles.Splatoon2.clientAppID;
+      break;
+    default:
+      clientAppID = 0;
+  }
+
+  return clientAppID;
+}
+
 // Sets the text of the dropdown menu to what the user selected
 function setTitle(title) {
   const titleName = document.getElementById('titleName');
   titleName.innerHTML = title;
-  titleName.value = title;
+  titleName.value = getclientAppID(title); //clientAppID
 }
 
 // Restores the initial values on the form
 function resetValues() {
   document.getElementById('titleName').innerHTML = '<i class="fas fa-gamepad"></i>&nbsp;&nbsp;Select your game';
+  document.getElementById('titleName').value = 0;
 }
