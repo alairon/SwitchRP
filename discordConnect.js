80a697df963945d9a92a4613de9a8e70625b8810
alairon <alai01@uoguelph.ca>
2019-01-11 12:08:40 -0500

    Adjusted variables and comments

--- discordConnect.js
+++ discordConnect.js
@@ -1,5 +1,7 @@
-/** For info on creating application IDs, visit
-* https://discordapp.com/developers/applications/
+/** discordConnect.js
+ * Functions used to update the user's status
+ * For info on creating application IDs, visit
+ * https://discordapp.com/developers/applications/
 */
 
 function adjustAppID(appID) {
@@ -12,14 +14,12 @@ function adjustAppID(appID) {
   return (clientAppID);
 }
 
-/* eslint-disable */
 // An object that holds clientAppID, details, and largeImageKey
 const appPresence = {
-  'clientAppID': adjustAppID(process.argv[2]),
-  'details': process.argv[3],
-  'largeImageKey': process.argv[4]
+  clientAppID: adjustAppID(process.argv[2]),
+  details: process.argv[3],
+  largeImageKey: process.argv[4],
 };
-/* eslint-enable */
 
 // Configures the user status with information passed in from the user
 const client = require('discord-rich-presence')(appPresence.clientAppID);
@@ -36,6 +36,5 @@ updatePresence();
 /* Process Functions */
 // Uncaught Rejection, in case of Discord Timeouts
 process.on('unhandledRejection', (reason) => {
-  console.log(appPresence);
   console.log(reason.stack || reason);
 });
