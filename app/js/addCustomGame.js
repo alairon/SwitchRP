b70eb0a9869f93a1813d107646015c03fb658df2
alairon <alai01@uoguelph.ca>
2019-01-18 20:56:57 -0500

    Refined custom entries

--- app/js/addCustomGame.js
+++ app/js/addCustomGame.js
@@ -1,6 +1,10 @@
+/* eslint no-unused-vars: 0 */
+
 const fs = require('fs');
 
-const gameJSONFile = '../titles.json';
+const gameJSONFile = 'app/titles.json';
+
+const fsErrMsg = document.getElementById('fsErrMsg');
 
 function removeSymbols(longName) {
   // Unicode Regex -- https://stackoverflow.com/a/25575009
@@ -14,13 +18,44 @@ function removeSymbols(longName) {
 function writeToJSON(newGameObj) {
   const gameJSON = fs.readFileSync(gameJSONFile);
   const updatedJSON = JSON.parse(gameJSON);
+
   updatedJSON[removeSymbols(newGameObj.longName)] = newGameObj;
 
-  fs.writeFile('test.json', JSON.stringify(updatedJSON, null, 2), (errException) => {
+  fs.writeFile(gameJSONFile, JSON.stringify(updatedJSON, null, 2), (errException) => {
     if (errException !== null) {
-      console.log(`${errException}`);
+      fsErrMsg.innerHTML = (`${errException}`);
+    } else {
+      document.getElementById('newGameName').value = '';
+      document.getElementById('newGameID').value = '';
+      document.getElementById('newLargeImageKey').value = '';
+      fsErrMsg.classList.remove('text-danger');
+      fsErrMsg.classList.add('text-success');
+      fsErrMsg.innerHTML = (`Successfully added "${newGameObj.longName}"`);
     }
   });
 }
 
-writeToJSON('../titles.json');
+function insertToJSON() {
+  const newGameObj = {
+    longName: document.getElementById('newGameName').value,
+    clientAppID: document.getElementById('newGameID').value,
+    largeImageKey: document.getElementById('newLargeImageKey').value,
+  };
+
+  writeToJSON(newGameObj);
+}
+
+function submitJSON() {
+  fsErrMsg.classList.remove('text-success');
+  fsErrMsg.classList.add('text-danger');
+
+  if (document.getElementById('newGameName').value === '') {
+    document.getElementById('fsErrMsg').innerHTML = ('The "Game Name" field cannot be empty');
+  } else if (document.getElementById('newGameID').value === '') {
+    document.getElementById('fsErrMsg').innerHTML = ('The "Game ID" field cannot be empty');
+  } else if (document.getElementById('newLargeImageKey').value === '') {
+    document.getElementById('fsErrMsg').innerHTML = ('The "Large Image Key" field cannot be empty');
+  } else {
+    insertToJSON();
+  }
+}
